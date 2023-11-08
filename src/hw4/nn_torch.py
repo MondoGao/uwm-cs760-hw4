803011db3bbc2e5b35e51dae5639ebc99167993c
Mondo <mondo.jiang@wisc.edu>
2023-11-08 12:35:34 -0600

    feat(q4_3): wip code forget to submit

---
+++ src/hw4/nn_torch.py
@@ -0,0 +1,12 @@
+import torch
+
+class Net(torch.nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+        self.fc1 = torch.nn.Linear(input_size, hidden_size)
+        self.fc2 = torch.nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        x = torch.sigmoid(self.fc1(x))
+        x = torch.softmax(self.fc2(x), dim=0)
+        return x
